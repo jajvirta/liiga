2c39210748bd08fafa0da343bfa94e92325d5bbd
Jarno Virtanen <jarno.virtanen@solita.fi>
2016-03-13 16:08:33 +0200

    gson-tuki

--- src/main/java/fi/tfs/liiga/LiigaApplication.java
+++ src/main/java/fi/tfs/liiga/LiigaApplication.java
@@ -2,7 +2,13 @@ package fi.tfs.liiga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 @SpringBootApplication
 
@@ -11,5 +17,14 @@ public class LiigaApplication {
     public static void main(String[] args) {
 
         ApplicationContext ctx = SpringApplication.run(LiigaApplication.class, args);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public GsonHttpMessageConverter gsonHttpMessageConverter(Gson defaultGson) {
+        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
+
+        converter.setGson(new GsonBuilder().create());
+        return converter;
     }
 }
