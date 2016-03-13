168d03ebc436e1fb06fd1cf7d67aae07f449844d
Jarno Virtanen <jarno.virtanen@solita.fi>
2016-03-13 22:39:08 +0200

    autentikointi toistaiseksi päälle koko saittiin

--- src/main/java/fi/tfs/liiga/LiigaApplication.java
+++ src/main/java/fi/tfs/liiga/LiigaApplication.java
@@ -3,18 +3,32 @@ package fi.tfs.liiga;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 @SpringBootApplication
-
-public class LiigaApplication {
+@EnableOAuth2Sso
+@EnableWebSecurity
+@Configuration
+public class LiigaApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(LiigaApplication.class, args);
+    }
+   
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .antMatcher("/**")
+            .authorizeRequests().anyRequest().authenticated();
     }
 
     @Bean
