d0152557478544b2e7c0320cc4cf629db3a6bc41
Jarno Virtanen <jajvirta@gmail.com>
2016-03-14 23:36:03 +0200

    csrf-token posteihin ja putteihin

--- front/app/common/api-util.js
+++ front/app/common/api-util.js
@@ -44,6 +44,8 @@ export default {
         return fetch(url, options)
             .then(function(response) {
                 var proposedCsrfToken = response.headers.get('XSRF-TOKEN');
+
+                console.log('calculating xsrf..', csrfToken, proposedCsrfToken);
                 if (proposedCsrfToken !== undefined && proposedCsrfToken && proposedCsrfToken !== csrfToken) {
                     csrfToken = proposedCsrfToken;
                 }
--- src/main/java/fi/tfs/liiga/LiigaApplication.java
+++ src/main/java/fi/tfs/liiga/LiigaApplication.java
@@ -1,5 +1,14 @@
 package fi.tfs.liiga;
 
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -10,6 +19,12 @@ import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -28,7 +43,33 @@ public class LiigaApplication extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .antMatcher("/**")
-            .authorizeRequests().anyRequest().authenticated();
+            .authorizeRequests().anyRequest().authenticated()
+            .and().csrf().csrfTokenRepository(csrfTokenRepository())
+            .and().addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+    }
+    
+    private CsrfTokenRepository csrfTokenRepository() {
+    	HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+    	repository.setHeaderName("X-XSRF-TOKEN");
+    	return repository;
+    }
+
+    private Filter csrfHeaderFilter() {
+    	return new OncePerRequestFilter() {
+    		@Override
+    		protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+    				FilterChain filterChain) throws ServletException, IOException {
+    			CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+    			if (csrf != null) {
+    				String attribute = request.getHeader("XSRF-TOKEN");
+    				String token = csrf.getToken();
+    				if (attribute == null || token != null && !token.equals(attribute)) {
+    					response.setHeader("XSRF-TOKEN", token);
+    				}
+    			}
+    			filterChain.doFilter(request, response);
+    		}
+    	};
     }
 
     @Bean
