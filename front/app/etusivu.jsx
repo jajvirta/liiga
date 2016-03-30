971d27b69c46ef38e8fa901766ab43eb94d56dfb
Jarno Virtanen <jajvirta@gmail.com>
2016-03-30 21:56:27 +0300

    kuvaus myös etusivulle

--- front/app/etusivu.jsx
+++ front/app/etusivu.jsx
@@ -7,6 +7,36 @@ export default React.createClass({
     render: function () {
         return (
             <div>
+
+                <h1>Mikä on Tampereen seudun frisbeegolf-joukkueliiga?</h1>
+
+                <p>
+                Tampereen seudun frisbeegolf-joukkueliiga on <b>kaikille avoin</b>
+                &nbsp;Tampereen seudun <b>joukkuepelisarja</b>, jossa etsitään Tampereen
+                seudun parasta frisbeegolf-joukkuetta. Joukkueen saa muodostaa vapaasti
+                eikä yhdenkään joukkueen jäsenen tarvitse kuulua esimerkiksi mihinkään
+                jäsenseuraan.  Joukkue valitsee itselleen kotiradan.
+                Kotiottelut pelataan lähtökohtaisesti joukkueen kotiradalla.
+                </p>
+
+                <p>
+                Joukkueet pelaavat kauden aikana sarjan, jossa joukkue pelaa
+                kaikkia muita joukkueita vastaan koti- ja vierasottelun.
+                Yhdessä ottelussa <b>neljän pelaajaan joukkueet</b> ratkovat
+                paremmuuden yhdeksän väylän tulospelissä ja yhdeksän väylän
+                reikäpeleissä.
+                </p>
+
+                <p>
+                Joukkueissa voi olla kauden aikana <b>vapaavalintainen määrä pelaajia</b>, mutta
+                yksittäiset ottelut pelataan aina nelihenkisesti. Kauden
+                lopuksi eniten pisteitä keränneet joukkueet palkitaan. Sarjan
+                järjestää ensimmäisellä kaudella Tampereen Frisbeeseura Ry.
+                </p>
+
+
+                <hr/>
+
                 <h1>Ajankohtaista frisbeegolf-joukkueliigasta</h1>
 
                 <h2><i>ke 22.3.2016</i>: Säännöt jotakuinkin valmiina, mutta niitä muutetaan tarvittaessa</h2>
