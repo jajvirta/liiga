7f0c0e423b9b60efdde2c6a08d533142dc3ca704
Jarno Virtanen <jarno.virtanen@solita.fi>
2016-07-09 11:34:31 +0300

    syysohjelma bäkkäriin

--- src/main/java/fi/tfs/liiga/ottelu/OtteluController.java
+++ src/main/java/fi/tfs/liiga/ottelu/OtteluController.java
@@ -1,9 +1,6 @@
 package fi.tfs.liiga.ottelu;
 
-import java.time.LocalDate;
-import java.util.GregorianCalendar;
 import java.util.List;
-import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,15 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import fi.tfs.liiga.ottelu.dto.TulevaOttelu;
-import net.fortuna.ical4j.model.Calendar;
-import net.fortuna.ical4j.model.DateTime;
-import net.fortuna.ical4j.model.TimeZoneRegistry;
-import net.fortuna.ical4j.model.TimeZoneRegistryFactory;
-import net.fortuna.ical4j.model.component.VEvent;
-import net.fortuna.ical4j.model.component.VTimeZone;
-import net.fortuna.ical4j.model.property.CalScale;
-import net.fortuna.ical4j.model.property.ProdId;
-import net.fortuna.ical4j.model.property.Version;
 
 @RestController
 public class OtteluController {
@@ -38,53 +26,18 @@ public class OtteluController {
         return otteluService.haeTulevatOttelut(joukkueId);
     }
 
-    private DateTime getEntry(int month, int day, int hour) {
-        java.util.Calendar cal = java.util.Calendar.getInstance();
-        cal.set(java.util.Calendar.MONTH, month-1);
-        cal.set(java.util.Calendar.DAY_OF_MONTH, day);
-        cal.set(java.util.Calendar.HOUR_OF_DAY, hour);
-        cal.clear(java.util.Calendar.MINUTE);
-        cal.clear(java.util.Calendar.SECOND);
-        cal.setTimeZone(TimeZone.getTimeZone("Europe/Helsinki"));
-        
-        DateTime dt = new DateTime(cal.getTime());
-        
-        return dt;
+    @RequestMapping(value="/public-api/liiga/{lohkoId}/ottelut/syksy/{joukkueId}", method=RequestMethod.GET)
+    public List<TulevaOttelu> tulevatOttelutSyksy(@PathVariable long lohkoId, @PathVariable long joukkueId) {
+        return otteluService.haeTulevatOttelut(joukkueId, "2016-07-01");
     }
 
+    @RequestMapping(value="/public-api/liiga/joukkue/{joukkueId}/kalenteri/syksy", produces="text/calendar")
+    public String haeJoukkueenSyyskalenteri(@PathVariable Long joukkueId) {
+        return otteluService.teeSyyskalenteri(joukkueId);
+    }
     @RequestMapping(value="/public-api/liiga/joukkue/{joukkueId}/kalenteri", produces="text/calendar")
     public String haeJoukkueenKalenteri(@PathVariable Long joukkueId) {
-        Calendar calendar = new Calendar();
-        calendar.getProperties().add(new ProdId("-//Ben Fortuna//iCal4j 1.0//EN"));
-        calendar.getProperties().add(Version.VERSION_2_0);
-        calendar.getProperties().add(CalScale.GREGORIAN);
-        
-        TimeZoneRegistry registry = TimeZoneRegistryFactory.getInstance().createRegistry();
-
-        VTimeZone tz = registry.getTimeZone("Europe/Helsinki").getVTimeZone();
-        calendar.getComponents().add(tz); 
-        
-       
-        List<TulevaOttelu> ottelut = otteluService.haeTulevatOttelut(joukkueId);
-        for (TulevaOttelu ottelu : ottelut) {
-            java.util.Calendar c = new GregorianCalendar();
-            c.setTime(ottelu.pelipaiva);
-            // LocalDate of = 
-            LocalDate of = java.time.LocalDate.of(c.get(java.util.Calendar.YEAR), c.get(java.util.Calendar.MONTH)+1, c.get(java.util.Calendar.DAY_OF_MONTH));
-
-
-            DateTime start = getEntry(c.get(java.util.Calendar.MONTH)+1, c.get(java.util.Calendar.DAY_OF_MONTH), 18);
-            DateTime end = getEntry(c.get(java.util.Calendar.MONTH)+1, c.get(java.util.Calendar.DAY_OF_MONTH), 20);
-
-            VEvent ve = new VEvent(start, end, ottelu.kotijoukkue + " vs. " + ottelu.vierasjoukkue + " (" + ottelu.kotirata + ")");
-            calendar.getComponents().add(ve);
-        }
-       
-
-        System.out.println("Printataan kalenteri " + joukkueId);
-        // System.out.println(calendar.toString());
-
-        return calendar.toString();
+        return otteluService.teeKalenteri(joukkueId);
     }
 }
 
--- src/main/java/fi/tfs/liiga/ottelu/OtteluDao.java
+++ src/main/java/fi/tfs/liiga/ottelu/OtteluDao.java
@@ -14,7 +14,15 @@ import fi.tfs.liiga.ottelu.dto.TulevaOttelu;
 @Component
 public class OtteluDao {
 
-    private final JdbcTemplate jdbcTemplate;
+     private final JdbcTemplate jdbcTemplate;
+     
+     private static final String baseQuery = 
+                " select o.ottelu_id, lj.lohkojoukkue_lohko_id lohko_id, o.pelipaiva, kj.joukkue_id kj_id, kj.kotirata, "
+                + "kj.nimi kotinimi, vj.joukkue_id vj_id, vj.nimi vierasnimi, o.kotijoukkue_pisteet kpiste, o.vierasjoukkue_pisteet vpiste "
+                + " from ottelu o "
+                + " join joukkue kj on (o.kotijoukkue_id = kj.joukkue_id) "
+                + " join joukkue vj on (o.vierasjoukkue_id = vj.joukkue_id) "
+                + " join lohko_joukkue lj on (o.kotijoukkue_id = lj.lohkojoukkue_joukkue_id) ";
 
     @Autowired
     public OtteluDao(JdbcTemplate jdbcTemplate) {
@@ -40,27 +48,12 @@ public class OtteluDao {
 
 
     public List<TulevaOttelu> haeTulevatOttelut() {
-        List<TulevaOttelu> query = jdbcTemplate.query(
-                " select o.ottelu_id, lj.lohkojoukkue_lohko_id lohko_id, o.pelipaiva, kj.joukkue_id kj_id, kj.kotirata, "
-                + "kj.nimi kotinimi, vj.joukkue_id vj_id, vj.nimi vierasnimi, o.kotijoukkue_pisteet kpiste, o.vierasjoukkue_pisteet vpiste "
-                + " from ottelu o "
-                + " join joukkue kj on (o.kotijoukkue_id = kj.joukkue_id) "
-                + " join joukkue vj on (o.vierasjoukkue_id = vj.joukkue_id) "
-                + " join lohko_joukkue lj on (o.kotijoukkue_id = lj.lohkojoukkue_joukkue_id) "
-                + " order by (o.pelipaiva, o.ottelu_id)",
-                new Mapper());
-
-        return query;
+        return jdbcTemplate.query(baseQuery + " order by (o.pelipaiva, o.ottelu_id)", new Mapper());
     }
 
     public List<TulevaOttelu> haeTulevatOttelut(long joukkueId) {
         List<TulevaOttelu> query = jdbcTemplate.query(
-                " select o.ottelu_id, lj.lohkojoukkue_lohko_id lohko_id, o.pelipaiva, kj.joukkue_id kj_id, kj.kotirata, "
-                + "kj.nimi kotinimi, vj.joukkue_id vj_id, vj.nimi vierasnimi, o.kotijoukkue_pisteet kpiste, o.vierasjoukkue_pisteet vpiste"
-                + " from ottelu o "
-                + " join joukkue kj on (o.kotijoukkue_id = kj.joukkue_id) "
-                + " join joukkue vj on (o.vierasjoukkue_id = vj.joukkue_id) "
-                + " join lohko_joukkue lj on (o.kotijoukkue_id = lj.lohkojoukkue_joukkue_id)"
+                baseQuery 
                 + " where (o.kotijoukkue_id = ? or o.vierasjoukkue_id = ?) "
                 + " order by (o.pelipaiva, o.ottelu_id)",
                 new Mapper(), joukkueId, joukkueId);
@@ -68,5 +61,14 @@ public class OtteluDao {
         return query;
     }
 
+    public List<TulevaOttelu> haeTulevatOttelut(long joukkueId, String earliestDate) {
+        List<TulevaOttelu> query = jdbcTemplate.query(
+                baseQuery 
+                + " where (o.kotijoukkue_id = ? or o.vierasjoukkue_id = ?)"
+                + " and o.pelipaiva > to_date(?, 'YYYY-MM-DD')" 
+                + " order by (o.pelipaiva, o.ottelu_id)",
+                new Mapper(), joukkueId, joukkueId, earliestDate);
 
+        return query;
+    }
 }
