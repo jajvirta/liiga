4e16c8c6e318e7cef535bd7c74f2dbf3eb8d8bb7
Jarno Virtanen <jarno.virtanen@solita.fi>
2016-07-09 11:33:59 +0300

    syysohjelma

---
+++ src/main/resources/db/migration/V15__syysohjelma.sql
@@ -0,0 +1,161 @@
+
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-07-20', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Woodpeckers'), (select joukkue_id from joukkue where nimi = 'VlkDG'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-07-20', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Aviaattorit'), (select joukkue_id from joukkue where nimi = 'Hervanta Disc Golf Team'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-07-20', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'DG Keijot'), (select joukkue_id from joukkue where nimi = 'HV'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-07-27', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Woodpeckers'), (select joukkue_id from joukkue where nimi = 'DG Keijot'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-07-27', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'VlkDG'), (select joukkue_id from joukkue where nimi = 'HV'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-07-27', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Hervanta Disc Golf Team'), (select joukkue_id from joukkue where nimi = 'Kiakkogosset'));
+
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-03', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'HV'), (select joukkue_id from joukkue where nimi = 'Aviaattorit'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-03', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Hervanta Disc Golf Team'), (select joukkue_id from joukkue where nimi = 'DG Keijot'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-03', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Kiakkogosset'), (select joukkue_id from joukkue where nimi = 'VlkDG'));
+
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-10', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'HV'), (select joukkue_id from joukkue where nimi = 'Woodpeckers'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-10', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Aviaattorit'), (select joukkue_id from joukkue where nimi = 'Kiakkogosset'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-10', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'VlkDG'), (select joukkue_id from joukkue where nimi = 'Hervanta Disc Golf Team'));
+
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-17', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Hervanta Disc Golf Team'), (select joukkue_id from joukkue where nimi = 'HV'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-17', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'DG Keijot'), (select joukkue_id from joukkue where nimi = 'Aviaattorit'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-17', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Kiakkogosset'), (select joukkue_id from joukkue where nimi = 'Woodpeckers'));
+
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-24', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'DG Keijot'), (select joukkue_id from joukkue where nimi = 'VlkDG'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-24', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'HV'), (select joukkue_id from joukkue where nimi = 'Kiakkogosset'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-24', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Aviaattorit'), (select joukkue_id from joukkue where nimi = 'Woodpeckers'));
+
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-31', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Woodpeckers'), (select joukkue_id from joukkue where nimi = 'Hervanta Disc Golf Team'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-31', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Kiakkogosset'), (select joukkue_id from joukkue where nimi = 'DG Keijot'));
+
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(1, to_date('2016-08-31', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'VlkDG'), (select joukkue_id from joukkue where nimi = 'Aviaattorit'));
+
+
+
+
+
+
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-07-20', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Ylöjärven Ryhti'), (select joukkue_id from joukkue where nimi = 'Sasta FG'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-07-20', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Brutal Force'), (select joukkue_id from joukkue where nimi = 'Amisketju'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-07-20', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Maajoukkue 2'), (select joukkue_id from joukkue where nimi = 'Nelosketju'));
+
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-07-28', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Sasta FG'), (select joukkue_id from joukkue where nimi = 'Maajoukkue 2'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-07-27', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Amisketju'), (select joukkue_id from joukkue where nimi = 'Ylöjärven Ryhti'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-07-28', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Nelosketju'), (select joukkue_id from joukkue where nimi = 'Brutal Force'));
+
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-08-04', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Nelosketju'), (select joukkue_id from joukkue where nimi = 'Sasta FG'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-08-03', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Maajoukkue 2'), (select joukkue_id from joukkue where nimi = 'Amisketju'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-08-03', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Brutal Force'), (select joukkue_id from joukkue where nimi = 'Ylöjärven Ryhti'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-08-11', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Sasta FG'), (select joukkue_id from joukkue where nimi = 'Brutal Force'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-08-10', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Ylöjärven Ryhti'), (select joukkue_id from joukkue where nimi = 'Maajoukkue 2'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-08-10', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Amisketju'), (select joukkue_id from joukkue where nimi = 'Nelosketju'));
+
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-08-17', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Amisketju'), (select joukkue_id from joukkue where nimi = 'Sasta FG'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-08-18', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Nelosketju'), (select joukkue_id from joukkue where nimi = 'Ylöjärven Ryhti'));
+
+insert into ottelu (ottelun_sarja_id, pelipaiva, kotijoukkue_id, vierasjoukkue_id) values 
+(2, to_date('2016-08-17', 'YYYY-MM-DD'), 
+(select joukkue_id from joukkue where nimi = 'Maajoukkue 2'), (select joukkue_id from joukkue where nimi = 'Brutal Force'));
+
