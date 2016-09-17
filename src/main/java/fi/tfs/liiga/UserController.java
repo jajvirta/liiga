151394c1cf461e0e0904681928ce48b8a9b411a4
Jarno Virtanen <jarno.virtanen@solita.fi>
2016-09-17 20:47:01 +0300

    parametrisoitu oauthid

--- src/main/java/fi/tfs/liiga/UserController.java
+++ src/main/java/fi/tfs/liiga/UserController.java
@@ -3,6 +3,7 @@ package fi.tfs.liiga;
 import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,10 @@ import fi.tfs.liiga.dto.UserInfo;
 @RestController
 @Configuration
 public class UserController {
-    
+   
+    @Value("${superuser.oauth_id}")
+    private String oauthId;
+
     @Autowired
     private CurrentUserUtil current;
 
@@ -22,7 +26,7 @@ public class UserController {
         OAuth2Authentication oauth = (OAuth2Authentication) principal;
         if (oauth != null && oauth.isAuthenticated()) {
             return new UserInfo(true, 
-                    current.getOauth2Id() != null && "10207383003566885".equals(current.getOauth2Id()), 
+                    current.getOauth2Id() != null && oauthId.equals(current.getOauth2Id()), 
                     current.getRealname());
         } else {
             return new UserInfo(false, false, null); 
